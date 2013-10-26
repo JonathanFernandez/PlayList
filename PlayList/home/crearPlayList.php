100decb90aaeb6f6327a71372c578e0445dccaad
SilviaKlodi <silviaklodi@yahoo.com.ar>
2013-10-26 20:52:11 -0300

    crearP

--- PlayList/home/crearPlayList.php
+++ PlayList/home/crearPlayList.php
@@ -1,3 +1,11 @@
+<?php
+	if(isset($_SESSION['finish']))
+		if($_SESSION['finish'] == true)
+			{
+				echo "error de logueo";
+				$_SESSION['finish'] = false;
+			}
+?>
 <!	DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" >
 <html  xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -12,11 +20,12 @@
 $(function() {
     $( "#menu" ).menu();
   });
+
 </script>
 
 </head>
 <body>
-<form action=""method="post" enctype="">
+<form action="subirPlaylist.php"method="post">
 <table border="0" style="width:100%;">
 	<tr>
 		<td colspan="2">
@@ -28,13 +37,41 @@ $(function() {
 			<?php include ('menu.php'); ?>
 		</td>
 		<td>
-			<!--div id="divCrearPlayList">
+			<div id="divCrearPlayList">
+				<div class="contenedor" id="contenedor1">
+					<div class="tags" id="tag1">
+						Nombre: 
+					</div>
+					<div class="inputs" id="input1">
+						<input type="text" name="nomPlaylist" id="nomPlaylist" size="35"/>
+					</div>
+				</div>
 				
-			<input name="archivo" type="file" size="35" />
-			<input name="enviar" type="submit" value="Upload File" />
-			<input name="action" type="hidden" value="upload" /-->     
-</form>
+				<div class="contenedor" id="contenedor2">
+					<div class="tags" id="tag2">
+						Categor&iacute;a: 
+					</div>
+					<div class="inputs" id="input2" >	
+						<input type="text" name="categoria" id="categoria" size="35"/>
+					</div>
+				</div>
 				
+				<div class="contenedor" id="contenedor3">
+					<div class="tags" id="tag3">
+						Privacidad: 
+					</div>
+					<div class="inputs" id="input3">	
+						<input type="radio" name="privacidad" value="1" checked> publica
+						<input type="radio" name="privacidad" value="2"> privada
+						<input type="radio" name="privacidad" value="3"> compartida
+					</div>
+				</div>
+				
+				<div class="submit" id="submit">
+					<input type="submit" value="Crear"/>
+				</div>
+			</div>
+			
 		</td>
 	</tr>
 
@@ -44,7 +81,7 @@ $(function() {
 
 
 
-</div>
+
 
 </form>
 </body>
