5295ffbdf20ec3266f32deea8d54d5fc5e59f7df
Facundo <fadcus@gmail.com>
2013-11-17 17:14:41 -0300

    pagina de loguin
    liso pagina de login

--- PlayList/home/login.php
+++ PlayList/home/login.php
@@ -11,36 +11,36 @@ function registrarse()
 }
 </script>
 </head>
-<body>
-<form action="validarUsuario.php" method="post">
-<?php
-	/*if(isset($_SESSION['nombre']) && isset($_SESSION['pass']))
-	{	
-		unset($_SESSION['nombre']);// session_destroy();
-		unset($_SESSION['pass']);
-	}*/
-	session_start();
-?>
-<div id="divlog" class="log">
-	<div class="log1">
-		E-Mail: </br>
-		Password: </br>
-	</div>
-	<div class="log2">
-		<input type="text" name="eMail" id="eMail"/></br>
-		<input type="password" name="pass" id="pass"/></br>
-	</div>
-	<input type="submit" value="Aceptar" class="boton"/>
-	<input type="button" value="Registrarse" onclick="javascript:registrarse();"/>
-</div>
-<?php
-	if(isset($_SESSION['finish']))
-		if($_SESSION['finish'] == true)
-			{
-				echo "error de logueo";
-				$_SESSION['finish'] = false;
-			}
-?>
-</form>
-</body>
+	<body>
+		<form action="validarUsuario.php" method="post">
+			<?php
+				/*if(isset($_SESSION['nombre']) && isset($_SESSION['pass']))
+				{	
+					unset($_SESSION['nombre']);// session_destroy();
+					unset($_SESSION['pass']);
+				}*/
+				session_start();
+			?>
+			<div id="divlog" class="log">
+				<div class="log1">
+					E-Mail: </br>
+					Password: </br>
+				</div>
+				<div class="log2">
+					<input type="text" name="eMail" id="eMail"/></br>
+					<input type="password" name="pass" id="pass"/></br>
+				</div>
+				<input type="submit" value="Aceptar" class="boton"/>
+				<input type="button" value="Registrarse" class="boton" onclick="javascript:registrarse();"/>
+			</div>
+			<?php
+				if(isset($_SESSION['finish']))
+					if($_SESSION['finish'] == true)
+						{
+							echo "error de logueo";
+							$_SESSION['finish'] = false;
+						}
+			?>
+		</form>
+	</body>
 </html>
