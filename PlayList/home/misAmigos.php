bb4ba810c9fa294cf126ec3faf870f403d7e8148
JonathanFernandez <jonathanerik.fernandez@gmail.com>
2013-11-17 19:57:44 -0300

    subo , ver mi amigos

---
+++ PlayList/home/misAmigos.php
@@ -0,0 +1,61 @@
+<?php
+	session_start();
+?>
+<!	DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd" >
+<html  xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+
+	<title>Mis Amigos</title>
+	<script type="text/javascript" src="../jQuery/jquery-1.9.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/styles.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/menuStyles.css"/>
+	<script src="../jQuery/1.10.3jquery-ui.js"></script>
+
+	<script type="text/javascript">
+	$(function() {
+		$( "#menu" ).menu();
+	  });
+	$(function() {
+		$( "#accordion" ).accordion();
+	  });
+
+	</script>
+	</head>
+	<body>
+		<form action="" method="post" enctype="" onsubmit="">
+
+			<?php include ('headerMenu.php'); ?>
+	
+			<?php include ('menu.php'); ?>
+			
+			<div id="divBuscarMisAmigos" class="buscarAmigos">
+						
+				<table>
+					<tr>
+						<td>
+							<input type="text" name="txtNombreAmigo" id="txtNombreAmigo"/>
+						</td>
+						<td>
+							<input type="submit" name="btnBuscarMiAmigo" id="btnBuscarMiAmigo" value="Buscar"/>
+						</td>
+					</tr>
+				</table>
+				<table>
+					<?php 
+							if(isset($_POST['btnBuscarMiAmigo']))
+							{
+								$like = "and (u.nombre like '%".$_POST['txtNombreAmigo']."%' or u.apellido like '%".$_POST['txtNombreAmigo']."%' 
+										 or u.alias like '%".$_POST['txtNombreAmigo']."%')";								
+							}
+							else
+							{
+								$like = "";
+							}
+						include('listarMisAmigos.php');	
+							
+					?>
+				</table>
+			</div>
+		</form>
+	</body>
+</html>
