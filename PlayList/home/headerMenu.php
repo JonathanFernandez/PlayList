443fc6566d94e9a015ac06215292e5063d94fdad
JonathanFernandez <jonathanerik.fernandez@gmail.com>
2013-11-23 17:17:33 -0300

    arreglo del boton salir

--- PlayList/home/headerMenu.php
+++ PlayList/home/headerMenu.php
@@ -1,4 +1,12 @@
+<script type="text/javascript">
+function Salir()
+{
+	var page = 'salir.php';
+	location.href = page;
+}
+</script>
+
 <div id="header" class="header">
 	<img src="..\images\logo.jpg" alt="logo" height="93%"/>
-	<input type="submit" value="Salir" id="btnSalir" name="btnSalir" class="boton" onclick = "this.form.action = 'salir.php'"/>
+	<input type="button" value="Salir" id="btnSalir" name="btnSalir" class="boton" onclick ='javascript:Salir()'/> 
 </div>
